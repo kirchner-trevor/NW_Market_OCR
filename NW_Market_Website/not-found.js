e19eb327e554637c7a8ae9547926ceb21d90a76a
Trevor <kirchner.trevor@gmail.com>
2021-11-19 20:09:21 -0600

    Fix title and not-found.

--- NW_Market_Website/not-found.js
+++ NW_Market_Website/not-found.js
@@ -2,7 +2,6 @@ export default {
     name: 'not-found',
     template: /*html*/`
     <div>
-        <page-header></page-header>
         <p>Sorry! The page you were looking for couldn't be found. Weird!</p>
     </div>
     `
