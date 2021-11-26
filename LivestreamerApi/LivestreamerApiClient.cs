9d981e78c3632b4b1bb74f66c347dcf95598c1e1
Trevor <kirchner.trevor@gmail.com>
2021-11-25 19:57:20 -0600

    Added livestreamer project.

---
+++ LivestreamerApi/LivestreamerApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LivestreamerApi
+{
+    public class LivestreamerApiClient
+    {
+        public void Test()
+        {
+            using (Process process = Process.Start(Path.Combine("Livestreamer", "livestreamer.exe"), ""))
+            {
+                process.WaitForExit();
+
+                string result = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ NW_Stream_Collector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NW_Stream_Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
